b80f01b7a56053f84f0634a900494c186caf9803
Pranav Navghare <pranavnavghare46@gmail.com>
2025-06-07 20:59:11 +0530

    tmdb-config.js

--- tmdb-config.js
+++ tmdb-config.js
@@ -1,3 +1,3 @@
 
 // tmdb-config.js
-const TMDB_API_KEY = 'YOUR_TMDB_API_KEY'; // Replace this with your real TMDb API key
+const TMDB_API_KEY = 'e9a17461bdd0167ca5182c0100084870'; // Replace this with your real TMDb API key
